123b09b5d7c17e4b379080360ef940fa31722c06
Matthew Knight <mgk1795@mail.com>
2019-07-29 07:36:08 -0700

    added visibility to bitfield offset

--- include/svd-alias/bit-field.hpp
+++ include/svd-alias/bit-field.hpp
@@ -8,21 +8,24 @@
 
 #include <cstdint>
 
-template <auto address, auto offset, auto width, typename T = std::uint32_t>
+template <auto address, auto position, auto width, typename T = std::uint32_t>
 struct BitFieldReadOnly {
     constexpr static auto max = (1 << width) - 1;
-    constexpr static auto mask = max << offset;
+    constexpr static auto mask = max << position;
+    constexpr static auto offset = position;
 
     volatile static T read() {
-        return (*reinterpret_cast<T*>(address) & mask) >> offset;
+        return (*reinterpret_cast<T*>(address) & mask) >> position;
     }
 };
 
-template <auto address, auto offset, auto width, typename T = std::uint32_t>
-struct BitField : public BitFieldReadOnly<address, offset, width, T> {
-    using BitFieldReadOnly<address, offset, width, T>::mask;
+template <auto address, auto position, auto width, typename T = std::uint32_t>
+struct BitField : public BitFieldReadOnly<address, position, width, T> {
+    using BitFieldReadOnly<address, position, width, T>::mask;
+    using BitFieldReadOnly<address, position, width, T>::offset;
+
     volatile static void write(const T& val) {
         auto ptr = reinterpret_cast<T*>(address);
-        *ptr = (*ptr & ~mask) | (mask & (val << offset));
+        *ptr = (*ptr & ~mask) | (mask & (val << position));
     }
 };
