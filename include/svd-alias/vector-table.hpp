063afb443664ca4f52752d2787f452d642716c50
Matthew Knight <mgk1795@mail.com>
2019-08-07 07:25:59 -0700

    moved vector table here

---
+++ include/svd-alias/vector-table.hpp
@@ -0,0 +1,50 @@
+// Interrupt Vector Table
+//
+// Author: Matthew Knight
+// File Name: interrupt-table.hpp
+// Date: 2019-08-02
+
+#pragma once
+
+#include <tuple>
+
+namespace Svd {
+    using Isr = void (*)();
+
+    std::int32_t const irqOffset = 16;
+
+    template <auto size, std::size_t alignment>
+    struct VectorTableBase {
+        __attribute__((aligned(alignment))) volatile Isr table[size];
+    };
+
+    template <typename Mcu>
+    using VectorTableCortexM =
+        VectorTableBase<Mcu::numInterrupts + irqOffset,
+                        1 << Mcu::SCB::VTOR::TBLOFF::offset>;
+
+    template <typename Mcu>
+    struct SystemExceptions;
+
+    template <typename Mcu>
+    struct VectorTable : public VectorTableCortexM<Mcu> {
+        using Base = VectorTableCortexM<Mcu>;
+        std::uint32_t const vtor;
+
+        template <typename Pair>
+        void loadPair(Pair pair) {
+            Base::table[static_cast<std::int32_t>(pair.first) + irqOffset] =
+                pair.second;
+        }
+
+        template <typename... Pairs>
+        VectorTable(Pairs&&... pairs)
+            : vtor(Mcu::SCB::VTOR::read()) {
+            // TODO: copy end of stack pointer, reset_handler, nmi_handler
+            (loadPair(pairs), ...);
+            Mcu::SCB::VTOR::write(reinterpret_cast<std::uint32_t>(Base::table));
+        }
+
+        ~VectorTable() { Mcu::SCB::VTOR::write(vtor); }
+    };
+} // namespace Svd
--- test_package/main.cpp
+++ test_package/main.cpp
@@ -1,5 +1,6 @@
 #include "svd-alias/svd-alias.hpp"
 #include "svd-alias/sys-tick.hpp"
+#include "svd-alias/vector-table.hpp"
 
 int main() {
     return 0;
